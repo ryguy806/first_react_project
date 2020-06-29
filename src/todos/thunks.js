37d87a9c9dd20edcdbed9d389ebaa514a1b1813c
ryguy806 <L1ghtSab3r11!!>
2020-06-29 13:01:03 -0700

    Updated dso new Todos are added to the server and persisted.

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,4 +1,4 @@
-import {loadTodosInProgree, loadTodosSuccess, loadTodosFailure} from "./actions";
+import { createTodo, loadTodosInProgree, loadTodosSuccess, loadTodosFailure } from "./actions";
 
 export const displayAlert = (text) => {
     alert(text);
@@ -14,5 +14,22 @@ export const loadTodos = () => async (dispatch, getState) => {
     }catch (e) {
         dispatch(loadTodosFailure());
         dispatch(displayAlert(e))
+    }
+}
+
+export const addTodoRequest = text => async dispatch => {
+    try {
+        const body = JSON.stringify({text});
+        const response = await fetch('http://localhost:8080/todos', {
+            headers: {
+                'Content-type': 'application/json',
+            },
+            method: post,
+            body,
+        });
+        const todo = await response.json();
+        dispatch(createTodo(todo));
+    } catch(e) {
+        dispatch(displayAlert(e));
     }
 }
