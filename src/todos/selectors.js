dcee6ab91285e93c97d0a559d738ef923c176ae4
ryguy806 <L1ghtSab3r11!!>
2020-06-29 14:32:02 -0700

    Updated so selectors can select only incomplete todos.

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,2 +1,9 @@
+import { createSelector } from 'reselect';
+
 export const getTodos = state => state.todos.data;
 export const getTodosLoading = state => state.data.isLoading;
+
+export const getIncompleteTodos = createSelector(
+    getTodos,
+    (todos) =>todos.filter(todo => !todo.isCompleted),
+)
